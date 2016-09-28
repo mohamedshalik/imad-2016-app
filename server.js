aa1aeede6ea20111146a6365209f5f402429ea80
Mohameds shalik <mohamedshalik@gmail.com>
2016-09-28 13:20:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var count=0;
+app.get('/count', function(req, res){
+    count+=1;
+    res.send(count.toString());
+});
+
 app.get('/Profile.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Profile.jpg'));
 });
